8831b5a1dff8f9b2759a13d5645719a52cfe5e7b
biosharp18 <rory.gao18@gmail.com>
2020-07-11 00:17:43 -0400

    added lat and long fields, all 5 description fields and title fields + added 3 test cases

--- fivespot/data/models.py
+++ fivespot/data/models.py
@@ -4,30 +4,51 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-	Name = models.CharField(max_length=100)
-	Email = models.EmailField(max_length=100)
-
+	name = models.CharField(max_length=100)
+	email = models.EmailField(max_length=100)
+	slug = models.SlugField(max_length=200, null=True)
 	def save(self, *args, **kwargs):
-		self.slug = slugify(self.Name)
+		self.slug = slugify(self.name)
 		super(User, self).save(*args, **kwargs)
 
 
 	def __str__(self):
-		return self.Name
+		return self.name
 
 	
 
 
 class Posts(models.Model):
 	user = models.ForeignKey(User, null=True)
-	Title = models.CharField(max_length=100)
-	Description = models.TextField(max_length=500)
+	title = models.CharField(max_length=100)
+	description = models.TextField(max_length=500)
 	created_at = models.DateTimeField(auto_now_add=True)
+	sub1 = models.CharField(max_length=100, null=True)
+	body1 = models.TextField(max_length=600, null=True)
+	lat1 = models.DecimalField(max_digits=22, decimal_places=16, null=True)
+	long1 = models.DecimalField(max_digits=22, decimal_places=16, null=True)
+	sub2 = models.CharField(max_length=100, null=True)
+	body2 = models.TextField(max_length=600, null=True)
+	lat2 = models.DecimalField(max_digits=22, decimal_places=16, null=True)
+	long2 = models.DecimalField(max_digits=22, decimal_places=16, null=True)
+	sub3 = models.CharField(max_length=100, null=True)
+	body3 = models.TextField(max_length=600, null=True)
+	lat3 = models.DecimalField(max_digits=22, decimal_places=16, null=True)
+	long3 = models.DecimalField(max_digits=22, decimal_places=16, null=True)
+	sub4 = models.CharField(max_length=100, null=True)
+	body4 = models.TextField(max_length=600, null=True)
+	lat4 = models.DecimalField(max_digits=22, decimal_places=16, null=True)
+	long4 = models.DecimalField(max_digits=22, decimal_places=16, null=True)
+	sub5 = models.CharField(max_length=100, null=True)
+	body5 = models.TextField(max_length=600, null=True)
+	lat5 = models.DecimalField(max_digits=22, decimal_places=16, null=True)
+	long5 = models.DecimalField(max_digits=22, decimal_places=16, null=True)
+	slug = models.SlugField(max_length=200, null=True)
 
 	def save(self, *args, **kwargs):
-		self.slug = slugify(self.Title)
+		self.slug = slugify(self.title)
 		super(Posts, self).save(*args, **kwargs)
 
 
 	def __str__(self):
-		return self.Title
+		return self.title
--- fivespot/data/serializers.py
+++ fivespot/data/serializers.py
@@ -4,11 +4,11 @@ from .models import Posts, User
 class PostsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Posts
-        fields = ('id', 'user', 'Title', 'Description')
+        fields = ('id', 'user', 'title', 'description', 'created_at', 'sub1', 'body1', 'lat1', 'long1', 'sub2', 'body2', 'lat2', 'long2', 'sub3', 'body3', 'lat3', 'long3', 'sub4', 'body4', 'lat4', 'long4', 'sub5', 'body5', 'lat5', 'long5')
     depth = 1
 
 class UsersSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
-		fields = ('id', 'Name', 'Email')  
+		fields = ('id', 'name', 'email')  
         
