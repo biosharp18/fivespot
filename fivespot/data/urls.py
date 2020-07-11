5bf2e788e5102b15c63b2711382b6cd71f79803d
biosharp18 <rory.gao18@gmail.com>
2020-07-10 22:31:13 -0400

    add api parameters for user posts (not there yet tho)

--- fivespot/data/urls.py
+++ fivespot/data/urls.py
@@ -2,8 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('api/posts/', views.PostsListCreate.as_view() ),
-    url('api/user1posts/', views.User1PostsListCreate.as_view()),
-    url('api/user2posts/', views.User2PostsListCreate.as_view()),
+    url('api/posts/', views.UserPostsListAPIView.as_view() ),
     url('api/users', views.UsersListCreate.as_view()),
 ]
--- fivespot/data/views.py
+++ fivespot/data/views.py
@@ -1,26 +1,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-#from django.shortcuts import render
+from django.shortcuts import render
 from .models import Posts, User
+from django.db.models import Q
 from .serializers import PostsSerializer, UsersSerializer
 from rest_framework import generics
 from rest_framework.generics import ListAPIView
-
-class PostsListCreate(generics.ListCreateAPIView):
-    queryset = Posts.objects.all()
-    serializer_class = PostsSerializer
+#class PostsListCreate(generics.ListCreateAPIView):
+  #  queryset = Posts.objects.all()
+  #  serializer_class = PostsSerializer
 
 class UsersListCreate(generics.ListCreateAPIView):
 	queryset = User.objects.all()
 	serializer_class = UsersSerializer
 # Create your views here.
-class User1PostsListCreate(generics.ListCreateAPIView):
-	queryset = Posts.objects.filter(user="1")
+class UserPostsListAPIView(generics.ListCreateAPIView):
 	serializer_class = PostsSerializer
-
-class User2PostsListCreate(generics.ListCreateAPIView):
-	queryset = Posts.objects.filter(user="2")
-	serializer_class = PostsSerializer
+	#queryset = Posts.objects.all()
+	def get_queryset(self, *args, **kwargs):	
+		queryset_list = Posts.objects.all()
+		query = self.request.GET.get("q")
+		if query:
+				queryset_list = queryset_list.filter(
+					Q(Title__icontains=query)).distinct()
+		return queryset_list
+	
 
 
 
