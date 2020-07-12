f2f08c4ad42db472957e5e680314986483eb6b86
biosharp18 <rory.gao18@gmail.com>
2020-07-12 02:38:00 -0400

    added marker

---
+++ fivespot/frontend/src/components/map/MapMarker.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap, Marker } from "react-google-maps";
+import CrayonGreen from './CrayonGreen';
+
+
+const center = {
+	lat: 45.42153,
+	lng: -75.697
+};
+
+
+function MarkerMap() {
+
+const[markers, setMarkers] = React.useState([]);
+
+	return (
+	<GoogleMap 
+	defaultZoom={10} 
+	defaultCenter={center}
+	defaultOptions={{ styles : CrayonGreen }}
+  onClick={(event) => {
+      setMarkers(current => [...current, {
+          lat: event.latLng.lat(),
+          lng: event.latLng.lng()
+      },
+      ]);
+    }}
+	>
+    {markers.map(marker => <Marker position={{ lat: marker.lat, lng: marker.lng }}/>)}
+    }
+  </GoogleMap>
+    
+);
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(MarkerMap));
+
+
+export class MapMarker extends Component {
+
+    render() {
+        return (
+               <div style={{ width: "50vw", height: "50vh" }}>
+               <WrappedMap 
+               googleMapURL={"https://maps.googleapis.com/maps/api/js?key=AIzaSyD0XoftgsSyzc2rnxugt6G2h3ap9-AOsgU"}
+               loadingElement={<div style={{ height: "100%" }}  /> }
+               containerElement={<div style={{ height: "100%" }} /> }
+               mapElement={<div style={{ height: "100%" }} /> }
+               />
+               </div>
+        );
+    }
+}
+
+export default MapMarker 
+
