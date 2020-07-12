3b59d1c746f8c13161d7c8c89972355eb6e3b7ab
biosharp18 <rory.gao18@gmail.com>
2020-07-11 23:29:37 -0400

    simple map

--- fivespot/frontend/src/components/map/Map.js
+++ fivespot/frontend/src/components/map/Map.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+import CrayonGreen from './CrayonGreen';
+
+
+const center = {
+	lat: 45.42153,
+	lng: -75.697
+};
+
+
+function Mapp() {
+	return (
+	<GoogleMap 
+	defaultZoom={10} 
+	defaultCenter={center}
+	defaultOptions={{style: CrayonGreen}}
+	/>
+);}
+
+const WrappedMap = withScriptjs(withGoogleMap(Mapp));
+
 
 export class Map extends Component {
+
     render() {
         return (
-            <div>
-                
-            </div>
-        )
+               <div style={{ width: "50vw", height: "50vh" }}>
+               <WrappedMap 
+               googleMapURL={"https://maps.googleapis.com/maps/api/js?key=AIzaSyD0XoftgsSyzc2rnxugt6G2h3ap9-AOsgU"}
+               loadingElement={<div style={{ height: "100%" }}  /> }
+               containerElement={<div style={{ height: "100%" }} /> }
+               mapElement={<div style={{ height: "100%" }} /> }
+               />
+               </div>
+        );
     }
 }
 
 export default Map
+
